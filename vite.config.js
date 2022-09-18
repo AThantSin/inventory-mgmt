9547966cbd0614c144f0f7b598b2348d53e308f7
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2022-09-18 12:06:04 +0630

    feat[breeze]: Build UI with Laravel breeze

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
- 
+
 export default defineConfig({
     plugins: [
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
-        ]),
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
     ],
-    server: {
-      https: true,
-      host: '0.0.0.0',
-    }
 });
